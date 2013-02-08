141853ab3d08a489a1323284d8cdf56788906704
Samuel Molnar <molnar.samuel@gmail.com>
2013-02-08 15:34:25 +0100

    Remove autostarts

--- awesome/autostart.lua
+++ awesome/autostart.lua
@@ -3,14 +3,8 @@
 do
 	local cmds = 
 	{ 
-		"gnome-screensaver",
-    "gnome-settings-daemon",
-    "gnome-power-manager",
-    "feh  --bg-scale '/home/samuel/.config/awesome/themes/niceandclean/wallpaper-1.jpg'",
-    "nm-applet",
     "pidgin",
 		"/home/samuel/.conky/startconky.sh",
-    "mpd"
 	}
 
 	for _,i in pairs(cmds) do
