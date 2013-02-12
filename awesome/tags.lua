6e0b7447dfb47b590a892bc431f22f5352bdbb13
Samuel Molnar <molnar.samuel@gmail.com>
2013-02-12 16:33:48 +0100

    Refactor tags

--- awesome/tags.lua
+++ awesome/tags.lua
@@ -12,42 +12,136 @@ layouts =
 
 -- Shifty: predefined tags
 shifty.config.tags   = {
-	["def"]        = { init = true, position = 1, layout = awful.layout.suit.floating        },
-	["doc"]    = { init = true, position = 2, layout = awful.layout.suit.floating  },
-	["web"] = { init = true, position = 3, layout = awful.layout.suit.max                      },
-	["1:work"]  = { init = true,  position = 4, layout = awful.layout.suit.tile                 },
-	["2:work"]  = { init = true, position = 5, layout = awful.layout.suit.tile              },
-	["3:work"]   = { init = true, position = 6, layout = awful.layout.suit.tile  },
-	["4:work"]   = { init = true, position = 7, layout = awful.layout.suit.tile  },
-	["5:work"]   = { init = true, position = 8, layout = awful.layout.suit.tile  },
-	["im"]= { init = true, position = 9, layout = awful.layout.suit.tile.left                     },
-	["music"]        = { init = false, position = 10, layout = awful.layout.suit.floating                      },
-	["gimp"]        = { init = false, position = 11, layout = awful.layout.suit.floating                     },
-	["video"]  = { position = 12, layout = awful.layout.suit.floating                                    },
-	["picture"]  = { layout = awful.layout.suit.max                                    },
-	["foo"] = { init = true, layout = awful.layout.suit.floating } 
+	["⌥"] = { 
+    init = true, 
+    position = 1, 
+    layout = awful.layout.suit.floating        
+  },
+	["⌤"]    = { 
+    init = true, 
+    position = 2, 
+    layout = awful.layout.suit.floating  
+  },
+	["❁"]= { 
+    init = true, 
+    position = 3, 
+    layout = awful.layout.suit.tile.left                     
+  },
+	["web"] = { 
+    init = true, 
+    position = 4, 
+    layout = awful.layout.suit.max                      
+  },
+	["1:work"]  = { 
+    init = true,  
+    position = 5, 
+    layout = awful.layout.suit.tile                 
+  },
+	["2:work"]  = { 
+    init = true,
+    position = 6,
+    layout = awful.layout.suit.tile    
+  },
+	["3:work"]   = {
+    init = true,
+    position = 7,
+    layout = awful.layout.suit.tile
+  },
+	["4:work"]   = {
+    init = true,
+    position = 8, 
+    layout = awful.layout.suit.tile 
+  },
+	["5:work"]   = {
+    init = true, 
+    position = 9, 
+    layout = awful.layout.suit.tile 
+  },
+	["music"]        = { 
+    init = false, 
+    position = 10,
+    layout = awful.layout.suit.floating      
+  },
+	["gimp"]        = { 
+    init = false,
+    position = 11, 
+    layout = awful.layout.suit.floating          
+  },
+	["video"]  = { 
+    position = 12,
+    layout = awful.layout.suit.floating         
+  },
+	["picture"]  = {
+    layout = awful.layout.suit.max        
+  },
+	["⌘"] = {
+    init = true, 
+    layout = awful.layout.suit.floating 
+  } 
 }
 
 -- Shifty: tags matching and client rules
 shifty.config.apps = {
 	-- net
-	{ match = { "Thunderbird", "Firefox"       }, tag = "web",                                               },
-	{ match = { "mutt"                   }, tag = "web",                                               },
+	{ match = { 
+    "Thunderbird", "Firefox"       }, 
+    tag = "web",                                               
+  },
+
+  { match = {
+    "mutt" }, 
+    tag = "web",         
+  },
 	--office
-	{ match = { "Libreoffice", "Lynx"   }, tag = "doc",                                            },
+	{ match = {
+    "Libreoffice", "Lynx"   },
+    tag = "doc",                               
+  },
 	-- gimp
-	{ match = { "Gimp"                   }, tag = "gimp",                                              },
-	{ match = { "gimp%-image%-window"    }, geometry = {176,15,929,800},                border_width = 0 },
-	{ match = { "^gimp%-toolbox$"        }, geometry = {0,15,175,800},    slave = true, border_width = 0 },
-	{ match = { "^gimp%-dock$"           }, geometry = {930,15,1280,800}, slave = true, border_width = 0 },
+	{ match = {
+    "Gimp" }, 
+    tag = "gimp",                                            
+  },
+
+  { match = { "gimp%-image%-window"    }, 
+  geometry = {176,15,929,800},               
+  border_width = 0
+},
+
+  { match = { "^gimp%-toolbox$"        },
+  geometry = {0,15,175,800},    
+  slave = true, 
+  border_width = 0 
+},
+
+	{ match = { "^gimp%-dock$" }, 
+  geometry = {930,15,1280,800},
+  slave = true, 
+  border_width = 0
+},
+
 	--video
-	{ match = { "MPlayer", "Vlc", "SMplayer"  }, tag = "video", geometry = {15,30,nil,nil}, float = true    },
+	{ match = { "MPlayer", "Vlc", "SMplayer"  }, 
+  tag = "video", 
+  geometry = {15,30,nil,nil}, 
+  float = true   
+},
 	-- music
-	{ match = { "audacious"  }, tag = "music",                                             },
+	{ match = { 
+    "audacious"  },
+    tag = "music",                                          
+},
 	-- im
-	{ match = { "irssi", "Pidgin", "Buddy List" }, tag = "im", float = true     },
+	{ match = { 
+    "irssi", "Pidgin", "Buddy List" },
+    tag = "❁", float = true     },
+
+
 	-- miscellaneous
-	{ match = { "sxiv", "gpicview", "feh"       }, tag = "picture",                                             },
+	{ match = {
+    "sxiv", "gpicview", "feh"       },
+    tag = "picture",                                            
+  },
 	
 	-- client manipulation
 	{ match = { "" },
